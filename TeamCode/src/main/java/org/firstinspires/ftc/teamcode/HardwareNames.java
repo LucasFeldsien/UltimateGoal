12f307137fd5c4cb83a214ef02452ff0c6b1e325
Lucas feldsien <lbfeldsien@gmail.com>
2020-10-28 20:15:49 -0700

    added more hardware names
    added servos and motors for the launcher, intake, storage, and added a placeholder for grabber and sensors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareNames.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareNames.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class HardwareNames {
+    // link to previous year's code https://github.com/teamftc8466/skystone8466
+    // link for learning coding https://www.w3schools.com/java/java_for_loop.asp
+    
     public DcMotor drivefr;
     public DcMotor drivefl;
     public DcMotor drivebr;
@@ -20,4 +24,53 @@ public class HardwareNames {
         drivebr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         drivebl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
+
+
+    public DcMotor launchmotor;
+    public Servo liftservo;
+    public Servo aimservo;
+    public Servo feedservo;
+
+    public void launcherMotors(HardwareMap hwm) {
+        launchmotor = hwm.get(DcMotor.class, "launcher");
+        liftservo = hwm.get(Servo.class, "liftservo");
+        aimservo = hwm.get(Servo.class, "aimservo");
+        feedservo = hwm.get(Servo.class, "feedservo");
+
+        /**drivefr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);**/
+    }
+
+
+    public DcMotor storgejectmotor;
+
+    public void storageMotors(HardwareMap hwm) {
+        storgejectmotor = hwm.get(DcMotor.class, "storgejectmotor");
+
+        /**drivefr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);**/
+    }
+
+
+    public DcMotor intakemotor;
+    /**MAYBE ANOTHER MOTOR NO CLUE**/
+
+    public void intakeMotors(HardwareMap hwm) {
+        storgejectmotor = hwm.get(DcMotor.class, "storgejectmotor");
+
+        /**drivefr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);**/
+    }
+
+
+    /**public idk grabber
+     *
+     * public void intakeMotors(HardwareMap hwm) {
+     *         storgejectmotor = hwm.get(DcMotor.class, "storgejectmotor");
+     *
+     *         drivefr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+     *}
+     */
+
+
+    /**public sensors
+     * public camera
+     */
 }
