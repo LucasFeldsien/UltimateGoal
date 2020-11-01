b3c0da8141b30b69f61c66429d699fbba5896b81
Lucas feldsien <lbfeldsien@gmail.com>
2020-11-01 13:48:26 -0800

    created Java class for drivetrain
    started a java class for drivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoDriveTrain.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.HardwareNames;
+
+class AutoDriveTrain {
+    //striaght (dist)
+    //turning (dist direction)
+    HardwareNames names = new HardwareNames();
+
+}
