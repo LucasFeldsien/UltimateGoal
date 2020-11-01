75547c2e19480ff803ff461bbdd92376f0a79bce
James Kang <jam2015k@gmail.com>
2020-11-01 15:07:31 -0800

    created autonomous program and added code
    created AutonomousCommon so that we can import everything from this file and.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoDriveTrain.java
@@ -1,12 +1,18 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.AutonomousCommon;
 import org.firstinspires.ftc.teamcode.HardwareNames;
 
-class AutoDriveTrain {
-    //striaght (dist)
-    //turning (dist direction)
-    HardwareNames names = new HardwareNames();
+@Autonomous(name = "BlueBuildingBackDoorStone", group = "FS")
+// @Disabled
+class AutonomousBlueBuildingFlank extends AutonomousCommon {
+    class AutoDriveTrain {
+        //striaght (dist)
+        //turning (dist direction)
+        HardwareNames names = new HardwareNames();
 
+    }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCommon.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+@Autonomous(name="AutonomousCommon")
+@Disabled
+public class AutonomousCommon extends HardwareNames {
+
+
+
+
+}
