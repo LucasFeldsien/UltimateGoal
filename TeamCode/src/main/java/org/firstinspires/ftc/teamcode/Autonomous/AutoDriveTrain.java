1d96a307f2d915c5c412159ca41ac642b91ad5a9
James Kang <jam2015k@gmail.com>
2020-11-01 15:13:48 -0800

    revert

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoDriveTrain.java
@@ -1,18 +1,12 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.AutonomousCommon;
 import org.firstinspires.ftc.teamcode.HardwareNames;
 
-@Autonomous(name = "BlueBuildingBackDoorStone", group = "FS")
-// @Disabled
-class AutonomousBlueBuildingFlank extends AutonomousCommon {
-    class AutoDriveTrain {
-        //striaght (dist)
-        //turning (dist direction)
-        HardwareNames names = new HardwareNames();
+class AutoDriveTrain {
+    //striaght (dist)
+    //turning (dist direction)
+    HardwareNames names = new HardwareNames();
 
-    }
 }
