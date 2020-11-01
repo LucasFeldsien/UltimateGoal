7d1129cab1387b52ab2691fa2a867a3b5b635d07
Lucas feldsien <lbfeldsien@gmail.com>
2020-11-01 14:21:16 -0800

    added code that allows pulling from hardware names
    the two drivetrain classes can now pull variables and methods from hardware names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoDriveTrain.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.HardwareNames;
 
-class AutoDriveTrain {
+class AutoDriveTrain extends HardwareNames {
+    public void driveMotors(HardwareMap hwm) {
+        super.driveMotors(hwm);
+    }
+    public void drivestraight() {
+    }
     //striaght (dist)
     //turning (dist direction)
-    HardwareNames names = new HardwareNames();
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpDriveTrain.java
@@ -1,6 +1,14 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-class TeleOpDriveTrain {
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.HardwareNames;
+
+class TeleOpDriveTrain extends HardwareNames {
+        public void driveMotors(HardwareMap hwm) {
+            super.driveMotors(hwm);
+        }
+
     //stright(stick input)
     //truning(stick input)
 }
