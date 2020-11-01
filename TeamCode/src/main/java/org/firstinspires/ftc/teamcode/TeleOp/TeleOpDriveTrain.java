907d791f6a6d12a2c2c2c75cb83eb2a33a27ab81
LiamAranda-Michel <51937885+LiamAranda-Michel@users.noreply.github.com>
2020-11-01 15:30:31 -0800

    laid out skeleton groundwork for up/down/strafe/diagonal movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpDriveTrain.java
@@ -9,6 +9,32 @@ class TeleOpDriveTrain extends HardwareNames {
             super.driveMotors(hwm);
         }
 
-    //stright(stick input)
-    //truning(stick input)
+    public void driveforward(float y) {
+    drivefr.setPower(y);
+    drivefl.setPower(y);
+    drivebr.setPower(y);
+    drivebl.setPower(y);
+    }
+    public void turn(float x) {
+            drivefl.setPower(x);
+            drivebl.setPower(x);
+            drivefr.setPower(-x);
+            drivebr.setPower(-x);
+    }
+    public void strafe(float x) {
+    drivefl.setPower(x);
+    drivefr.setPower(-x);
+    drivebl.setPower(-x);
+    drivebr.setPower(x);
+    }
+
+    public void diagonalright (float y) {
+        drivefl.setPower(y);
+        drivebr.setPower(y);
+    }
+
+    public void diagonalleft (float y) {
+            drivefr.setPower(-y);
+            drivebl.setPower(-y);
+    }
 }
