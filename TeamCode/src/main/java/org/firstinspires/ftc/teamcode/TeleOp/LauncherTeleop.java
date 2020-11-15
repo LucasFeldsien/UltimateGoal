18d9fc75ffdc1b0cd1ec02d0ec33c91d72138b17
LiamAranda-Michel <51937885+LiamAranda-Michel@users.noreply.github.com>
2020-11-15 13:27:22 -0800

    first complete iteration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LauncherTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LauncherTeleop.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
-import java.lang.Math;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.HardwareNames;
@@ -10,55 +9,46 @@ class LauncherTeleop extends HardwareNames {
     }
     //Method for on/off toggle, method for adjusting strength, method for tilt, method for launching rings ;)
     public void LauncherToggle (boolean leftbumper) {
-        if (leftbumper == true) {
+        if (leftbumper) {
             launchmotor.setPower(1);
         }
     }
     //need to change second launchmotor for krabby flip
-        public void Eject (boolean rightbumper) {
-            if (rightbumper == true) {
-                launchmotor.setPower(1);
+        public void Eject (double launcherpower, boolean rightbumper) {
+            if (rightbumper) {
+                launchmotor.setPower(launcherpower);
             }
         }
 
-         public void launcherpower (boolean buttonA, boolean buttonB, boolean buttonX, boolean buttonY) {
-                if (buttonA == true) {
-                    launchmotor.setPower(.25);
+         public double launcherpower (double power, boolean buttonA, boolean buttonB, boolean buttonX, boolean buttonY) {
+                if (buttonA) {
+                    power = .25;
                 }
 
-                else if (buttonX == true) {
-                    launchmotor.setPower(.50);
+                else if (buttonX) {
+                    power = .50;
                 }
 
-                else if (buttonY == true) {
-                    launchmotor.setPower(.75);
+                else if (buttonY) {
+                    power = .75;
                 }
 
-                else if (buttonB == true) {
-                    launchmotor.setPower(1);
+                else if (buttonB) {
+                    power = 1;
                 }
+                return power;
                 }
 
                 public void ejectangle (boolean lefttrigger, boolean righttrigger) {
-                       if (lefttrigger == true) {
-                           aimservo.getPosition();
-                           aimservo.setPosition(aimservo.getPosition() - 0.1);
-                       }
+                    while (lefttrigger) {
+                            aimservo.getPosition();
+                            aimservo.setPosition(aimservo.getPosition() - 0.1);
+                    }
 
-                        if (righttrigger == true) {
-                        aimservo.getPosition();
-                        aimservo.setPosition(aimservo.getPosition() + 0.1);
-                        }
-
+                    while (righttrigger) {
+                            aimservo.getPosition();
+                            aimservo.setPosition(aimservo.getPosition() + 0.1);
+                    }
                 }
 
-
             }
-
-
-
-
-
-
-
-
