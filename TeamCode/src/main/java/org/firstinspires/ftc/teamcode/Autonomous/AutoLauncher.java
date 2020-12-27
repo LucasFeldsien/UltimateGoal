16b4fb01bad0df59f71efe58b9c7b3ac499cc9b1
Lucas feldsien <lbfeldsien@gmail.com>
2020-12-27 14:44:27 -0800

    fixed servo name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoLauncher.java
@@ -11,7 +11,7 @@ class AutoLauncher extends HardwareNames {
         super.launcherMotors(hwm);
     }
     public void shoot(double vis) {
-        servoaim.setPosition(vis);
+        servolift.setPosition(vis);
         motorlaunch.setPower(1.0);
         servofeed.setPosition(1.0);//continous
         servolift.setPosition(1.0);
